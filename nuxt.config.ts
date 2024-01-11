3fc65a4c7507014d1ccf76825b7f8930cc03ad02
Twelve615 <321884950@qq.com>
2024-01-11 14:45:35 +0800

    :bug: 修复spa打包之后在utools中访问的css、js等文件是绝对路径的问题，以及首次进入访问的页面是绝对路径而找不到页面的问题

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -7,7 +7,16 @@ export default defineNuxtConfig({
   ],
 
   ssr: false,
-
+  app: {
+    // 解决spa构建之后，js、css等资源文件使用绝对路径而找不到的问题
+    baseURL: './',
+  },
+  router: {
+    options: {
+      // 解决spa构建之后在utools中首次进入时访问的路径是绝对路径的问题
+      hashMode: true
+    }
+  },
   experimental: {
     payloadExtraction: false,
     inlineSSRStyles: false,
